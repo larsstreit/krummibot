97e51473f9cee2de6825e73bd20f457c95ec5441
Lars <ls.larsstreit@t-online.de>
2022-01-18 18:43:40 +0100

    fix

--- app.js
+++ app.js
@@ -8,6 +8,20 @@ var users = {}
 var botusers = {}
 var joinedchannel = []
 
+const http = require('http');
+
+// Create an instance of the http server to handle HTTP requests
+let app = http.createServer((req, res) => {
+    // Set a response type of plain text for the response
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+
+    // Send back a response and end the connection
+    res.end('Hello World!\n');
+});
+
+// Start the server on port 3000
+app.listen(3000, '127.0.0.1');
+console.log('Node server running on port 3000');
 
 try {
     if (fs.existsSync(path)) {
