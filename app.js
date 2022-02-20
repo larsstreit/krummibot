19670702edab4d4e04c54e480ec3d0cc3cc4a5f0
Lars Streit <larsstreit@Air-von-Lars.fritz.box>
2022-02-20 19:02:28 +0100

    0.0.2

--- app.js
+++ app.js
@@ -5,48 +5,84 @@ const filepath = require('./path');
 const opts = require('./config');
 const commandHandler = require('./commandHandler')
 const bot = new tmi.client(opts);
-try {
-	if(fs.existsSync(filepath.botuserspath) && fs.existsSync(filepath.packagepath)) {
-		let botusersfile = fs.readFileSync(filepath.botuserspath);
-		objvar.botusers = JSON.parse(botusersfile);
-		let packagefile = fs.readFileSync(filepath.packagepath);
-		objvar.package = JSON.parse(packagefile);
-	}
-} catch (err) {
-	console.error(err);
+const express = require('express');
+const { botusers } = require('./var');
+prestart();
+
+function prestart(){
+    try {
+        if(fs.existsSync(filepath.botuserspath) && fs.existsSync(filepath.packagepath)) {
+            let botusersfile = fs.readFileSync(filepath.botuserspath);
+            objvar.botusers = JSON.parse(botusersfile);
+            let packagefile = fs.readFileSync(filepath.packagepath);
+            objvar.package = JSON.parse(packagefile);
+            startbot();
+            const app = express()
+            app.get('/user/:channel', (req, res) => {
+                req.params; 
+                console.log(req.params.channel);
+                if('#'+req.params.channel in objvar.botusers){
+                    res.json(objvar.botusers[`#${req.params.channel}`])
+                }
+                else{
+                    res.status(404)
+                    res.send("User not exist")
+                }
+            });
+            app.get('/users/', (req, res) => {
+                res.json(Object.keys(objvar.botusers))
+                res.end()
+
+                
+            });
+              
+              app.listen(3000)
+        }
+        else{
+            prestart();
+        }
+    } catch (err) {
+        console.error(err);
+    }
 }
 
-bot.connect().then(() => {
-	for (const [key, value] of Object.entries(objvar.botusers)) {
-		console.log(value);
-		console.log(key);
-		// shows the value of botusers[key] console.log(objvar.botusers[key]) does the same as console.log(value);
-		// key => #channelname etc. console.log(key)
-		if (value.joined === true) {
-			bot.join(key)
-				.then((data) => {
-					//shows which channel was joined
-					console.log(data);
-				}).catch((err) => {
-					console.log(err);
-				});
-			//need if bot gets shutdown on every channel
-			objvar.joinedchannel.push(key);
-			// setTimeout(async () => {
-			// 	await bot.say(key, `Hallo @${key.replace('#', '')}`);
-			// 	await bot.say(key, `testing ${objvar.package.name} version ${objvar.package.version}`);
-			// }, 2000);
-		}	
-	}
-}).catch(console.error);
 
-bot.on('message', messageHandler);
-bot.on('raided', raidHandler);
-bot.on('subscription', subscriptionHandler);
+function startbot(){
+    bot.connect().then(() => {
+        for (const [key, value] of Object.entries(objvar.botusers)) {
+            //console.log(value);
+            //console.log(key);
+            // shows the value of botusers[key] console.log(objvar.botusers[key]) does the same as console.log(value);
+            // key => #channelname etc. console.log(key)
+            if (value.joined === true) {
+                bot.join(key)
+                    .then((data) => {
+                        //shows which channel was joined
+                        console.log(data);
+                    }).catch((err) => {
+                        console.log(err);
+                    });
+                //need when bot gets shutdown on every channel
+                objvar.joinedchannel.push(key);
+            }	
+        }
+    }).catch(console.error);
+    
+    bot.on('message', messageHandler);
+    bot.on('raided', raidHandler);
+    bot.on('subscription', subscriptionHandler);
+    bot.on('anonsubmysterygift', subscriptionHandler)
+    
+}
 
 
-function  subscriptionHandler(channel, username, method, message, userstate){
+
+
+function subscriptionHandler(channel, username, method, message, userstate){
     console.log(channel, username, method, message, userstate);
+}
+function subscriptionHandler(channel, numbOfSubs, methods, userstate){
+    console.log(channel, numbOfSubs, methods, userstate);
 }
 function raidHandler(channel, raider, viewers) {
     bot.say(channel, `${raider}, raidet mit ${viewers} Flamingos`);
