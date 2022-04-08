5cdbf523e855427bde19538e7008d3401f1e1223
Lars Streit <larsstreit@Air-von-Lars.fritz.box>
2022-04-09 01:27:16 +0200

    0.0.2

---
+++ botconfig.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+module.exports =  {
+	options: {
+		debug: true,
+		messagesLogLevel: 'info'
+	},
+	connection: {
+		reconnect: true,
+		secure: true
+	},
+	identity: {
+		username: process.env.BOT_NAME,
+		password: process.env.BOT_OAUTH 
+	},
+	channels: ['krummibot']
+};
