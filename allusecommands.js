1765397bdce9fa55c80b4304e28b7c028f6c7ba8
Lars Streit <larsstreit@Air-von-Lars.fritz.box>
2022-04-15 14:48:48 +0200

    0.0.3

---
+++ allusecommands.js
@@ -0,0 +1,20 @@
+module.exports =  { 
+  allusecommands:  [
+  "!help",
+  "!channelcommands",
+  "!channelcommands help",
+  "!krummi",
+  "!so",
+  "!pokemon",
+  "!pokemon catch",
+  "!pokemon index",
+  "!pokemon help",
+  "!commands",
+  "!love",
+  "!games",
+  "!coin",
+  "!würfel",
+  "!miesmuschel",
+  "!hug"
+]
+}
